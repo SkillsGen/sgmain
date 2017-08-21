af2bd339019704e874edf6e16081aac9be0fa165
arc7722 <sreinolds@gmail.com>
2017-08-21 11:01:29 +0100

    debug false

--- application.py
+++ application.py
@@ -300,5 +300,5 @@ def search(message=""):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port,debug=False)
     
